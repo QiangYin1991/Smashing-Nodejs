e4c609e07800d48d08d24024c73eb5c46e075a3a
SOHU-INC\qiangyin <qiangyin@sohu-inc.com>
2018-12-05 18:20:14 +0800

    跑不起来呢

--- chapter12/user-auth-example/index.js
+++ chapter12/user-auth-example/index.js
@@ -1,6 +1,21 @@
-const express = require('express');
-const mongodb = require('mongodb');
+const express      = require('express');
+const mongodb      = require('mongodb');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session      = require('express-session');
 
 const app = express();
 
-app.use()
+app.use(bodyParser);
+app.use(cookieParser);
+app.use(session({secret: 'my secret'}));
+
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index', {authenticated: false});
+});
+
+app.listen(3000, function() {
+  console.log("server listened on 3000");
+})
