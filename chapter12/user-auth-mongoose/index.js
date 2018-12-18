befc878f95dc712ebdd4d1b2b959bc6d7e485664
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-18 21:59:37 +0800

    构建mongoose项目

---
+++ chapter12/user-auth-mongoose/index.js
@@ -0,0 +1,5 @@
+const express      = require('express');
+const mongodb      = require('mongodb');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session      = require('express-session');
