f985f43380ae7bb64daff0cca2540cc948148bba
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-20 22:35:17 +0800

    完善http请求

--- chapter8/http-server/index.js
+++ chapter8/http-server/index.js
@@ -2,13 +2,25 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function (req, res) {
-    if ('GET' === req.method && '/image' === req.url.substr(0, 7) && '.jpg' === req.url.substr(-4)) {
-
+    if ('GET' === req.method && '/images' === req.url.substr(0, 7) && '.jpg' === req.url.substr(-4)) {
+        fs.stat(__dirname + req.url, function (err, stat) {
+            if (err || !stat.isFile()) {
+                res.writeHead(404);
+                res.end('Not found');
+                return;
+            }
+        });
+        serve(__dirname + req.url, 'application/jpg')
     } else if ('GET' == req.method && '/' === req.url) {
-
+        serve(__dirname + '/index.html', 'text/html');
     } else {
         res.writeHead(404);
         res.end('Not found');
+    }
+
+    function serve(path, type) {
+        res.writeHead(200, {'Content-Type': type});
+        fs.createReadStream(path).pipe(res);
     }
 });
 
