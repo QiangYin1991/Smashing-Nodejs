36bbc7b7060eb4d6751a3d695c445abdf99ca098
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-22 23:59:43 +0800

    request-time实现

--- chapter8/connnect-server/request-time.js
+++ chapter8/connnect-server/request-time.js
@@ -1,3 +1,16 @@
 module.exports = function (opts) {
-    
+    const time = opts.time || 100;
+
+    return function (req, res, next) {
+        const timer = setTimeout(() => {
+            console.log('%s %s mis take too long', req.method, req.url);
+        }, time);
+        
+        const end = res.end;
+        res.end = function (chunk, encoding) {
+            res.end = end;
+            res.end(chunk, encoding);
+            clearTimeout(timer);
+        }
+    }
 }
