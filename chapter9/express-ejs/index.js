401111b4a95986918a2008dd90f46a3f7a2bedf6
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-24 23:31:31 +0800

    代码逻辑

--- chapter9/express-ejs/index.js
+++ chapter9/express-ejs/index.js
@@ -1,5 +1,18 @@
 const express = require('express');
-const search = require('./views/search');
 
-const app = express.createServer();
+const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view options', {layout:false});
+
+/**
+ * 路由
+ */
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.listen(3000, function () {
+    console.log('listen on ' + 3000);
+});
