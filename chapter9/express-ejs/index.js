cc530289107a1bc3092d51cf983e13a6d3d3e5f6
QiangYin1991 <qiangyin1991@gmail.com>
2018-12-25 22:36:58 +0800

    添加superagent调用

--- chapter9/express-ejs/index.js
+++ chapter9/express-ejs/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const search = require('./search');
 
 const app = express();
 
@@ -11,6 +12,13 @@ app.set('view options', {layout:false});
  */
 app.get('/', function (req, res) {
     res.render('index');
+});
+
+app.get('/search', function (req, res, next) {
+    search(req.query.q, function (err, tweets) {
+        if (err) return next(err);
+        res.render('search', {results: tweets, search: req.query.q});
+    });
 });
 
 app.listen(3000, function () {
