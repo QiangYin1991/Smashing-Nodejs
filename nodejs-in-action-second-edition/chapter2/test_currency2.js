a557b120e3522d15cfa43e7231f9ce31d2a625c9
QiangYin1991 <qiangyin1991@gmail.com>
2019-01-05 14:43:49 +0800

    创建子目录

--- nodejs-in-action-second-edition/chapter2/test_currency2.js
+++
@@ -1,7 +0,0 @@
-const Currency = require('./currency2');
-
-const canadianDollar = 0.91;
-const currency = new Currency(canadianDollar);
-
-console.log('50 Canadian dollars equals this amount of US dollars:' + currency.canadianToUS(50));
-console.log('30 US dollars equals this amount of Canadian dollars:' + currency.USToCanadian(30));
