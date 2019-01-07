04be47872aeb31b1b3912ba0c11cec3faf8be7a2
QiangYin1991 <qiangyin1991@gmail.com>
2019-01-07 22:32:28 +0800

    完善chapter3

--- nodejs-in-action-second-edition/chapter3/index.js
+++ nodejs-in-action-second-edition/chapter3/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const articles = [{title: 'Example'}];
+const Article = require('./db').Article;
+const read = require('node-readability');
 
 app.set('port', process.env.PORT || 3000);
 
@@ -9,26 +10,38 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/articles', (req, res, next) => {
-    res.send(articles);
+    Article.all((err, articles) => {
+        if (err) return next(err);
+        res.send(articles);
+    });
 });
 
 app.post('/articles', (req, res, next) => {
-    const article = {title: req.body.title};
-    articles.push(article);
-    res.send(article);
+   const url = req.body.url;
+   read(url, (err, result) => {
+       if (err || !result) res.status(500).send('Error downloading article');
+       console.log(`title:${result.title} content:${result.content}`);
+       Article.create({title: result.title, content: result.content}, (err, artcle) => {
+           if (err) return next(err);
+           res.send('OK');
+       })
+   })
 });
 
 app.get('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Fetching:', id);
-    res.send(articles[id]);
+    Article.find(id, (err, article) => {
+        if (err) return next(err);
+        res.send(article);
+    });
 });
 
 app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
-    console.log('Deleting:', id);
-    delete articles[id];
-    res.send({message: 'Deleted'});
+    Article.delete(id, (err) => {
+        if (err) return next(err);
+        res.send({message: 'Deleted'});
+    })
 });
 
 app.listen(app.get('port'), () => {
