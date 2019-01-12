e5e7046548ce330c59a0e8e00183bc24abc5d2df
QiangYin1991 <qiangyin1991@gmail.com>
2019-01-12 22:00:16 +0800

    完善部分逻辑

--- nodejs-in-action-second-edition/chapter6/shoutbox/app.js
+++ nodejs-in-action-second-edition/chapter6/shoutbox/app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -37,5 +37,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.get('/post', entries.form);
+app.post('/post', entries.submit);
 
 module.exports = app;
